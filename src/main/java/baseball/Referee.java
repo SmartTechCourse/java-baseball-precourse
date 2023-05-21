7f33ed4ba22574cf56ebcb9e1cd395822e2cb9c6
손병재 <byeongjae.dev@gmail.com>
2023-05-21 17:06:47 +0900

    feat : 입력한 수와 상대방(컴퓨터)의 수를 비교하여 결과를 출력하는 기능 구현

---
+++ src/main/java/baseball/Referee.java
@@ -0,0 +1,74 @@
+package baseball;
+
+import java.util.List;
+
+public class Referee {
+
+    private boolean isAllMatched = false;
+
+    public void match(List<Integer> targetNumberDigits, List<Integer> inputNumberDigits) {
+        int numberOfBall = countBall(targetNumberDigits, inputNumberDigits);
+        int numberOfStrike = countStrike(targetNumberDigits, inputNumberDigits);
+
+        printMatchingResult(numberOfBall, numberOfStrike);
+        setMatchResult(numberOfStrike);
+    }
+
+    public boolean getMatchResult() {
+        return isAllMatched;
+    }
+
+    public void initializeMatchResult() {
+        isAllMatched = false;
+    }
+
+    private void setMatchResult(int numberOfStrike) {
+        if (numberOfStrike == 3) {
+            isAllMatched = true;
+        }
+    }
+
+    private int countBall(List<Integer> targetNumberDigits, List<Integer> inputNumberDigits) {
+        int numberOfBall = 0;
+
+        // TODO: 인덴트 2이하로 줄이기.
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                if (i != j) {
+                    if (targetNumberDigits.get(i) == inputNumberDigits.get(i)) {
+                        numberOfBall += 1;
+                    }
+                }
+            }
+        }
+
+        return numberOfBall;
+    }
+
+    private int countStrike(List<Integer> targetNumberDigits, List<Integer> inputNumberDigits) {
+        int numberOfStrike = 0;
+
+        // TODO: 인덴트 2이하로 줄이기.
+        for (int i = 0; i < 3; i++) {
+            if (targetNumberDigits.get(i) == inputNumberDigits.get(i)) {
+                numberOfStrike += 1;
+            }
+        }
+
+        return numberOfStrike;
+    }
+
+    private void printMatchingResult(int numberOfBall, int numberOfStrike) {
+        if (numberOfBall > 0) {
+            System.out.print(numberOfBall +"볼 ");
+        }
+        if (numberOfStrike > 0) {
+            System.out.print(numberOfStrike +"스트라이크");
+        }
+        else{
+            System.out.print("낫싱");
+        }
+        System.out.println();
+    }
+
+}
