9918865be6a0be92ebd5323c4df2357873b8a9de
손병재 <byeongjae.dev@gmail.com>
2023-05-21 16:35:21 +0900

    Refactor: validator 통합 및 로직 수정

--- src/main/java/baseball/TypeConvertingValidator.java
+++
@@ -1,9 +0,0 @@
-package baseball;
-
-public class TypeConvertingValidator implements Validator {
-
-    public static Boolean canConvertStringToInteger(String str) {
-        return str != null && str.matches("[0-9]+");
-    }
-
-}
--- src/main/java/baseball/Validator.java
+++ src/main/java/baseball/Validator.java
@@ -1,4 +1,19 @@
 package baseball;
 
-public interface Validator {
+import java.util.List;
+
+public class Validator {
+
+    public static boolean isInRange(List<Integer> inputDigits) {
+        if (inputDigits.size() != 3 || inputDigits.contains(0)) {
+            return false;
+        }
+
+        return true;
+    }
+
+    public static Boolean canConvertStringToInteger(String str) {
+        return str != null && str.matches("[0-9]+");
+    }
+
 }
