03a7c209d945af406da0b2f88a3fbc8cd209f3ef
손병재 <byeongjae.dev@gmail.com>
2023-05-21 17:10:29 +0900

    Refactor: if문 단순화

--- src/main/java/baseball/Validator.java
+++ src/main/java/baseball/Validator.java
@@ -5,11 +5,7 @@ import java.util.List;
 public class Validator {
 
     public static boolean isInRange(List<Integer> inputDigits) {
-        if (inputDigits.size() != 3 || inputDigits.contains(0)) {
-            return false;
-        }
-
-        return true;
+        return inputDigits.size() == 3 && !inputDigits.contains(0);
     }
 
     public static Boolean canConvertStringToInteger(String str) {
