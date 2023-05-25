83ba3be76e9d0c951f238d2db3581350d23821ed
손병재 <byeongjae.dev@gmail.com>
2023-05-26 02:09:47 +0900

    Feat: 상대방 숫자 반환 기능 추가

--- src/main/java/baseball/Opponent.java
+++ src/main/java/baseball/Opponent.java
@@ -1,10 +1,22 @@
 package baseball;
 
+import camp.nextstep.edu.missionutils.Randoms;
+import java.util.LinkedList;
 import java.util.List;
+import java.util.stream.IntStream;
 
 public class Opponent {
 
+    private List<Integer> number;
+
     public List<Integer> getNumber() {
+        if (number == null) {
+            generateNumber();
+        }
+        return number;
+    }
+
+    private void generateNumber() {
 
     }
 
