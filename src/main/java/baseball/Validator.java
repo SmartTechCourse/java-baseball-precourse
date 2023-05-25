48dd348c35de00a0b6360e05d06fc0a3e0ce946d
손병재 <byeongjae.dev@gmail.com>
2023-05-26 03:02:56 +0900

    Feat: 플레이어가 콘솔로 받은 입력이 숫자인지 판단하는 기능 추가

--- src/main/java/baseball/Validator.java
+++ src/main/java/baseball/Validator.java
@@ -1,11 +1,14 @@
 package baseball;
 
-import java.util.List;
-
 public class Validator {
 
     public static void validate(String input) {
-
+        checkTypeConverting(input);
     }
-    
+
+    private static void checkTypeConverting(String input) {
+        if (input == null || !input.matches("[0-9]+")) {
+            throw new IllegalArgumentException("입력값에 숫자가 아닌 다른 값이 포함돼있습니다. 숫자만 입력해주세요.");
+        }
+    }
 }
