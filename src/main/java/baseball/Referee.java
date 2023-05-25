148e28d0b2a1099378ba16c55c57574a9f930638
손병재 <byeongjae.dev@gmail.com>
2023-05-26 01:41:56 +0900

    Feat: 심판이 스트라이크와 볼 개수 판정하는 기능 추가

--- src/main/java/baseball/Referee.java
+++ src/main/java/baseball/Referee.java
@@ -1,13 +1,49 @@
 package baseball;
 
 import java.util.List;
+import java.util.stream.IntStream;
 
 public class Referee {
 
+    private Player player;
+
+    private Opponent computer;
+
+    public Referee(Player player, Opponent computer) {
+        this.player = player;
+        this.computer = computer;
+    }
     public JudgementResult judge() {
+        List<Integer> playerNumber = player.getNumber();
+        List<Integer> opponentNumber = computer.getTargetNumber();
+
+        return compare(playerNumber, opponentNumber);
     }
 
+    private JudgementResult compare(List<Integer> playerNumber, List<Integer> opponentNumber) {
+        int numberOfStrike = countStrike(playerNumber, opponentNumber);
+        int numberOfBall = countBall(playerNumber, opponentNumber);
+
+        return new JudgementResult(numberOfStrike, numberOfBall);
+    }
+
+    private int countStrike(List<Integer> playerNumber, List<Integer> opponentNumber) {
+        return (int) IntStream
+            .rangeClosed(0, 2)
+            .filter(i -> playerNumber.get(i) == opponentNumber.get(i))
+            .count();
+    }
+
+    private int countBall(List<Integer> playerNumber, List<Integer> opponentNumber) {
+        return (int) IntStream
+            .rangeClosed(0, 2)
+            .filter(playerNumberIndex -> {
+                int opponentNumberIndex = opponentNumber.indexOf(playerNumber.get(playerNumberIndex));
+                return opponentNumberIndex != playerNumberIndex && opponentNumberIndex != -1;})
+            .count();
+    }
 
     public void requestToChangeOpponentNumber() {
+
     }
 }
