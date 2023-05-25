83985e04a65bc2672ce289234d54055a48d41ae1
손병재 <byeongjae.dev@gmail.com>
2023-05-26 02:29:35 +0900

    Feat: 플레이어 숫자 반환 기능 추가

--- src/main/java/baseball/Player.java
+++ src/main/java/baseball/Player.java
@@ -5,9 +5,13 @@ import java.util.List;
 
 public class Player {
 
+    private List<Integer> number;
+
     public List<Integer> getNumber() {
-
+        String input = Console.readLine();
+        Validator.validate(input);
+        number = convertStringToIntegerList(input);
+        return number;
     }
-
 
 }
