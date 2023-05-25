740f8f9bb8dc0de53c9231580ec6e49c6da93f36
손병재 <byeongjae.dev@gmail.com>
2023-05-26 03:15:07 +0900

    Feat: 게임 종료 메세지와 게임 재시작 혹은 종료 여부를 입력하도록 출력하는 기능 추가

--- src/main/java/baseball/View.java
+++ src/main/java/baseball/View.java
@@ -7,5 +7,11 @@ public class View {
     }
 
     public void print(JudgementResult result) {
+
+    }
+
+    private void printGameOverMessage() {
+        System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
+        System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
     }
 }
