5a5ebf30ff82256665095f655281093006ead6ac
손병재 <byeongjae.dev@gmail.com>
2023-05-26 03:03:50 +0900

    Feat: 플레이어가 콘솔로 받은 입력에 중복된 수가 있는지 판단하는 기능 추가

--- src/main/java/baseball/Validator.java
+++ src/main/java/baseball/Validator.java
@@ -1,14 +1,28 @@
 package baseball;
 
+import java.util.Arrays;
+
 public class Validator {
 
     public static void validate(String input) {
         checkTypeConverting(input);
+        checkDuplication(input);
     }
 
     private static void checkTypeConverting(String input) {
         if (input == null || !input.matches("[0-9]+")) {
             throw new IllegalArgumentException("입력값에 숫자가 아닌 다른 값이 포함돼있습니다. 숫자만 입력해주세요.");
+        }
+    }
+
+    private static void checkDuplication(String input) {
+        long nonDuplicateWordCount = Arrays
+            .stream(input.split(""))
+            .distinct()
+            .count();
+
+        if (nonDuplicateWordCount != input.length()) {
+            throw new IllegalArgumentException("입력값에 중복이 포함돼있습니다. 중복되지 않는 숫자를 입력해주세요.");
         }
     }
 }
