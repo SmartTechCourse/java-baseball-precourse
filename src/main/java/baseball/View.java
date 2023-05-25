6423ab855b235942bd0e63345282f9373a090ff4
손병재 <byeongjae.dev@gmail.com>
2023-05-26 03:26:04 +0900

    Feat: 스트라이크와 볼 개수 혹은 낫싱을 출력하는 기능 추가

--- src/main/java/baseball/View.java
+++ src/main/java/baseball/View.java
@@ -7,7 +7,28 @@ public class View {
     }
 
     public void print(JudgementResult result) {
+        if (result.isMaxStrike()) {
+            printGameOverMessage();
+            return;
+        }
 
+        int numberOfBall = result.getNumberOfBall();
+        int numberOfStrike = result.getNumberOfStrike();
+        StringBuilder stringBuilder = new StringBuilder();
+
+        if (numberOfBall > 0) {
+            stringBuilder.append(numberOfBall+"볼 ");
+        }
+        if (result.getNumberOfStrike() > 0) {
+            stringBuilder.append(numberOfStrike+"스트라이크 ");
+        }
+        String JudgementResultMessage = stringBuilder.toString();
+
+        if (JudgementResultMessage.isEmpty()) {
+            System.out.println("낫싱");
+        } else {
+            System.out.println(JudgementResultMessage);
+        }
     }
 
     private void printGameOverMessage() {
