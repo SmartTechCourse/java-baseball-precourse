211d0661a90f9128dc705ae3bca6efbf6465891b
손병재 <byeongjae.dev@gmail.com>
2023-05-26 03:10:14 +0900

    Feat: 숫자 입력 메세지 출력 기능 추가

--- src/main/java/baseball/View.java
+++ src/main/java/baseball/View.java
@@ -3,11 +3,9 @@ package baseball;
 public class View {
 
     public void printNumberInputMessage() {
-
+        System.out.print("숫자를 입력해주세요 : ");
     }
 
     public void print(JudgementResult result) {
     }
-
-
 }
