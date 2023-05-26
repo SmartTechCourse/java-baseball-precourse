42d85b427dc6649d9c04c487e2173f3572a8e778
손병재 <byeongjae.dev@gmail.com>
2023-05-27 03:27:10 +0900

    Refactor: 게임 재시작, 종료 유즈케이스를 main에서 시작하도록 변경

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,9 +1,26 @@
 package baseball;
 
+import camp.nextstep.edu.missionutils.Console;
+
+import static baseball.Message.SIGNAL_INVALID_ERROR;
+
 public class Application {
     public static void main(String[] args) {
+        final String START_SIGNAL = "1";
+        final String CLOSE_SIGNAL = "2";
+
         Referee referee = new Referee();
         Game game = new Game(referee);
-        game.run();
+
+        String signal = START_SIGNAL;
+
+        while (signal.equals(START_SIGNAL)) {
+            game.start();
+            signal = Console.readLine();
+        }
+
+        if (!signal.equals(CLOSE_SIGNAL)) {
+            throw new IllegalArgumentException(SIGNAL_INVALID_ERROR.getBody());
+        }
     }
 }
--- src/main/java/baseball/Game.java
+++ src/main/java/baseball/Game.java
@@ -1,31 +1,22 @@
 package baseball;
 
-import camp.nextstep.edu.missionutils.Console;
-
-import static baseball.Message.*;
-
 public class Game {
-
     private Referee referee;
 
     private boolean isRunning;
-
-    private final String RESTART_SIGNAL = "1";
-
-    private final String CLOSE_SIGNAL = "2";
 
     public Game(Referee referee) {
         this.referee = referee;
     }
 
-    public void run() {
-        isRunning = true;
-
+    public void start() {
         Player player = new Player();
         Opponent computer = new Opponent();
 
+        isRunning = true;
+
         while (isRunning) {
-            View.printNumberInputMessage();
+            View.print(Message.INPUT_NUMBER_GUIDE);
             JudgementResult judgementResult = referee.judge(player, computer);
             View.print(judgementResult);
             checkKeepRunning(judgementResult);
@@ -34,27 +25,7 @@ public class Game {
 
     private void checkKeepRunning(JudgementResult judgementResult) {
         if (judgementResult.isMaxStrike()) {
-            checkRestartOrClose();
+            isRunning = false;
         }
-    }
-
-    private void checkRestartOrClose() {
-        String inputString = Console.readLine();
-
-        if (inputString.equals(RESTART_SIGNAL)) {
-            restart();
-        } else if (inputString.equals(CLOSE_SIGNAL)) {
-            close();
-        } else {
-            throw new IllegalArgumentException(RESTART_OR_CLOSE_SIGNAL_INVALID_ERROR.getBody());
-        }
-    }
-
-    private void restart() {
-        referee.requestToChangeNumber(computer);
-    }
-
-    private void close() {
-        isRunning = false;
     }
 }
